4b88f8149caf0d3e5e3542cc964942c3f9f0b091
antonifs <antoni.fs@gmail.com>
2016-06-26 23:34:51 +0700

    update model relation of item foreign key

--- items/admin.py
+++ items/admin.py
@@ -11,7 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'date_created', 'date_updated')
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('code', 'category_name', 'description', 'user_name', 'date_created', 'date_updated')
+    list_display = ('code',
+                    'category_name',
+                    'description',
+                    'stock_amount',
+                    'user_name',
+                    'date_updated'
+                    )
     search_fields = ('code', 'description')
     list_filter = ('category__name', 'date_created')
 
@@ -19,7 +25,11 @@ class PriceAdmin(admin.ModelAdmin):
     list_display = ('price', 'date_updated')
 
 class StockAdmin(admin.ModelAdmin):
-    list_display = ('amount', 'date_updated')
+    list_display = (
+                    'item_code',
+                    'amount',
+                    'date_updated',
+                    )
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Item, ItemAdmin)
--- items/models.py
+++ items/models.py
@@ -21,12 +21,15 @@ class Item(models.Model):
     def category_name(self):
         return self.category.name
 
+    def stock_amount(self):
+        stock = Stock()
+        return stock.cd(1)
+
     def user_name(self):
         return u'%s %s' % (self.user.first_name, self.user.last_name)
 
     def __str__(self):
         return self.code
-
 
 class Stock(models.Model):
     amount = models.IntegerField()
@@ -34,8 +37,14 @@ class Stock(models.Model):
     user = models.ForeignKey(User)
     item = models.ForeignKey(Item)
 
+    def item_code(self):
+        return self.item.code
+
+    def cd(self, id):
+        return self.objects.filter(id=1)
+
     def __str__(self):
-        return self.amount
+        return str(self.amount)
 
 class Price(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
@@ -43,5 +52,8 @@ class Price(models.Model):
     user = models.ForeignKey(User)
     item = models.ForeignKey(Item)
 
+    def item_code(self):
+        return self.item.code
+
     def __str__(self):
-        return self.price
+        return str(self.price)
