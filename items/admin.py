3530fabdf774ec2f1a98d61ee4e204fe73c4f967
antonifs <antoni.fs@gmail.com>
2016-07-07 22:22:59 +0700

    update actions button

--- items/admin.py
+++ items/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.core import serializers
 
 from .models import (
         Category,
@@ -10,11 +12,6 @@ from .models import (
 def update_all(modeladmin, request, queryset):
     queryset.update(price = 0)
 update_all.short_description = "Update selected items"
-
-def activate(modeladmin, request, queryset):
-    queryset.update(status = 1)
-    print "status updated"
-activate.short_description = "Activate selected items"
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'date_created', 'date_updated')
@@ -30,7 +27,28 @@ class ItemAdmin(admin.ModelAdmin):
                     )
     search_fields = ('code', 'category__name', 'description')
     list_filter = ('category__name', 'date_created')
-    actions = [activate]
+
+    def activate(self, request, queryset):
+        rows_updated = 0
+        for d in queryset.values():
+            if d['status'] == 1:
+                rows_updated = queryset.update(status = 1)
+                if rows_updated == 1:
+                    message_bit = "1 Item was"
+                else:
+                    message_bit = "%s Items were " % rows_updated
+        if rows_updated == 0:
+            self.message_user ("Item is being active already.")
+        else:
+            self.message_user (request, "%s successfully updated. " % rows_updated)
+
+    def export_as_json(self, request, queryset):
+        response = HttpResponse(content_type = "application/json")
+        serializers.serialize("json", queryset, stream=response)
+        return response
+
+    actions = [export_as_json, activate]
+
 
 class PriceAdmin(admin.ModelAdmin):
     list_display = (
